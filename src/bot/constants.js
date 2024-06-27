a45dd17c7dcc365626151c9a53ea9f815a28221e
Nikita <n.zagainov@innopolis.university>
2024-06-27 08:28:42 +0300

    adapt classifier prompt to new database scheme

--- src/bot/constants.js
+++ src/bot/constants.js
@@ -4,7 +4,7 @@ To reset chat history, type /start or /reset
 To display this message, type /help`;
 const CLASSIFIER_MESSAGE = `This is a system message.
 Your role is to classify the dialogue from below as one of the given intents.
-You will be provided with a list of intents in JSON format: [{"INTENT_NAME": "INTENT_DESCRIPTION"}...]. If the dialogue perfectly matches one of the descriptions, respond with that intent name: "INTENT_NAME".
+You will be provided with a list of intents in JSON format: [{"name": "INTENT_NAME", "description": "INTENT_DESCRIPTION", "id": INTENT_ID}]. If the dialogue perfectly matches one of the descriptions, respond with that intent name: "INTENT_NAME".
 If the dialogue does not perfectly match any of the descriptions (even if it is really close, but not exact), respond with "none".`;
 const FORGOTTEN_CHAT_MESSAGE = `This is a system message. 
 It seems that your client has left the conversation for a while. Try to get them back to the dialogue by analyzing their conversation history and resuming accordingly.`;
